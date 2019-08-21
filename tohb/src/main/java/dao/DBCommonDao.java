2afa760ee3eaf3afea98b14a2e9b61d7e3a87ae3
bro3rd <yudwang@outlook.com>
2019-08-21 09:31:43 +0800

    no

--- tohb/src/main/java/dao/DBCommonDao.java
+++ tohb/src/main/java/dao/DBCommonDao.java
@@ -1,4 +1,37 @@
 package dao;
 
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
+import java.util.List;
+
 public class DBCommonDao implements DBDaoIn {
+    @Override
+    public void insert(List<String> list) {
+
+    }
+
+    @Override
+    public void insertMany(List<List<String>> list, int batchSize) {
+
+    }
+
+    @Override
+    public ResultSet read(String key) {
+        return null;
+    }
+
+    @Override
+    public ResultSet readTable() {
+        return null;
+    }
+
+    @Override
+    public DatabaseMetaData getMetaData() {
+        return null;
+    }
+
+    @Override
+    public long test(List<List<String>> list, int batchSize) {
+        return 0;
+    }
 }
